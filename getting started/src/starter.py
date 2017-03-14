96c33c7bf4e88b1a7a7d3e86e1501d800ee2cda5
JohnGarethRoberts <gareth.spud@gmail.com>
2017-03-14 15:09:28 +0000

    python 3 env

---
+++ getting started/src/starter.py
@@ -0,0 +1,102 @@
+
+print("hello world")
+
+
+#%%
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import numpy as np
+
+x = np.linspace(0, 20, 100)
+plt.plot(x, np.sin(x))
+plt.show()
+
+
+#%%
+import matplotlib.pyplot as plt
+import numpy as np
+import mpld3
+
+mpld3.enable_notebook()
+fig, ax = plt.subplots(subplot_kw=dict(facecolor='#EEEEEE'))
+ax.grid(color='white', linestyle='solid')
+N = 50
+scatter = ax.scatter(np.random.normal(size=N),
+                     np.random.normal(size=N),
+                     c=np.random.random(size=N),
+                     s = 1000 * np.random.random(size=N),
+                     alpha=0.3,
+                     cmap=plt.cm.jet)
+ax.set_title("D3 Scatter Plot", size=18);
+
+
+#%%
+from bokeh.io import push_notebook, show, output_notebook
+from bokeh.layouts import row, gridplot
+from bokeh.plotting import figure, show, output_file
+output_notebook()
+
+import numpy as np
+
+x = np.linspace(0, 4*np.pi, 100)
+y = np.sin(x)
+TOOLS = "pan,wheel_zoom,box_zoom,reset,save,box_select"
+
+p1 = figure(title="Legend Example", tools=TOOLS)
+p1.circle(x,   y, legend="sin(x)")
+p1.circle(x, 2*y, legend="2*sin(x)", color="orange")
+p1.circle(x, 3*y, legend="3*sin(x)", color="green")
+show(p1)
+
+#%%
+from IPython.display import Latex
+Latex('''The mass-energy equivalence is described by the famous equation
+
+$$E=mc^2$$
+
+discovered in 1905 by Albert Einstein.
+In natural units ($c$ = 1), the formula expresses the identity
+
+\\begin{equation}
+E=m
+\\end{equation}''')
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import mpld3
+from mpld3 import plugins
+np.random.seed(9615)
+
+# generate df
+N = 100
+df = pd.DataFrame((.1 * (np.random.random((N, 5)) - .5)).cumsum(0),
+                  columns=['a', 'b', 'c', 'd', 'e'],)
+
+# plot line + confidence interval
+fig, ax = plt.subplots()
+ax.grid(True, alpha=0.3)
+
+for key, val in df.iteritems():
+    l, = ax.plot(val.index, val.values, label=key)
+    ax.fill_between(val.index,
+                    val.values * .5, val.values * 1.5,
+                    color=l.get_color(), alpha=.4)
+
+# define interactive legend
+
+handles, labels = ax.get_legend_handles_labels() # return lines and labels
+interactive_legend = plugins.InteractiveLegendPlugin(zip(handles,
+                                                         ax.collections),
+                                                     labels,
+                                                     alpha_unsel=0.5,
+                                                     alpha_over=1.5,
+                                                     start_visible=True)
+plugins.connect(fig, interactive_legend)
+
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_title('Interactive legend', size=20)
+
+mpld3.display()
